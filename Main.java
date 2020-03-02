8f12fe00431ac44f10ada9faa49176569979530a
shivamsahay0 <60652255+shivamsahay0@users.noreply.github.com>
2020-03-02 19:55:27 +0530

    Q1

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String str1= "hey this is new class";
+        String str2= str1.replace("new", "old");
+        System.out.println(str2);
+
+    }
+}
